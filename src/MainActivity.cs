6e72256602f25818b2a19b6ac31798d4c81a28bb
Trevor Balcom <tbalcom@herculesfreight.com>
2021-07-28 14:31:55 -0400

    Initial commit.

---
+++ src/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.OS;
+using AndroidX.AppCompat.App;
+using AndroidX.RecyclerView.Selection;
+using AndroidX.RecyclerView.Widget;
+
+namespace AndroidXRecyclerViewSelection
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private readonly ZipAdapter adapter = new ZipAdapter();
+        private SelectionTracker selectionTracker;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            var recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView1);
+            if (savedInstanceState == null)
+            {
+                recyclerView.SetLayoutManager(new LinearLayoutManager(this));
+                recyclerView.SetAdapter(adapter);
+            }
+
+            selectionTracker = new SelectionTracker.Builder("selectionId",
+                    recyclerView,
+                    new ZipKeyProvider(recyclerView),
+                    new ZipItemDetailsLookup(recyclerView),
+                    StorageStrategy.CreateLongStorage())
+                .WithSelectionPredicate(SelectionPredicates.CreateSelectAnything())
+                .Build();
+            adapter.SetTracker(selectionTracker);
+        }
+
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
+            if (outState != null)
+                selectionTracker?.OnSaveInstanceState(outState);
+        }
+    }
+}
---
+++ src/ZipItemDetails.cs
@@ -0,0 +1,20 @@
+﻿namespace AndroidXRecyclerViewSelection
+{
+    using AndroidX.RecyclerView.Selection;
+
+    public class ZipItemDetails : ItemDetailsLookup.ItemDetails
+    {
+        private readonly int adapterPosition;
+        private readonly long selectionKey;
+
+        public ZipItemDetails(int adapterPosition, long selectionKey)
+        {
+            this.adapterPosition = adapterPosition;
+            this.selectionKey = selectionKey;
+        }
+
+        public override int Position => adapterPosition;
+
+        protected override Java.Lang.Object RawSelectionKey => selectionKey;
+    }
+}
